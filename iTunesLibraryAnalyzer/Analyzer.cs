aa8d4815c66f23ab14bb3efbc180c2d76f8e242f
Simon Klimov <simon-klimov@yandex.ru>
2018-04-26 02:46:03 +0300

    Add project files.

---
+++ iTunesLibraryAnalyzer/Analyzer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace iTunesLibraryAnalyzer
+{
+	class Analyzer
+	{
+		private string xmlLibraryFilePathName, key;
+		public Dictionary<string, int> Results { get; private set; }
+
+		public Analyzer(string xmlLibraryFilePathName, string key)
+		{
+			this.xmlLibraryFilePathName = xmlLibraryFilePathName;
+			this.key = key;
+			Results = new Dictionary<string, int>();
+		}
+
+		public void Analyze()
+		{
+			try
+			{
+				XmlTextReader reader = new XmlTextReader(xmlLibraryFilePathName);
+				reader.WhitespaceHandling = WhitespaceHandling.None;
+				while (reader.Read())
+				{
+					int count;
+					if (reader.NodeType == XmlNodeType.Text && reader.Value == key)
+					{
+						reader.Read();
+						while (reader.NodeType != XmlNodeType.Text)
+						{
+							reader.Read();
+						}
+						if (Results.TryGetValue(reader.Value, out count))
+						{
+							Results[reader.Value] = count + 1;
+						}
+						else
+						{
+							Results[reader.Value] = 1;
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+	}
+}
---
+++ iTunesLibraryAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace iTunesLibraryAnalyzer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			tbLibraryFilePathName.Text = @"C:\Users\" + Environment.UserName + @"\Music\iTunes\iTunes Music Library.xml";
+		}
+
+		private void btnAnalyze_Click(object sender, RoutedEventArgs e)
+		{
+			Analyzer ana = new Analyzer(tbLibraryFilePathName.Text, cbKeyToAnalyze.Text);
+			ana.Analyze();
+
+			tbResults.Text = "Count\tValue\n";
+			foreach (KeyValuePair<string, int> item in ana.Results.OrderByDescending(key => key.Value))
+			{
+				tbResults.AppendText(item.Value.ToString() + "\t" + item.Key + "\n");
+			}
+		}
+
+		private void btnBrowse_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+			dialog.InitialDirectory = tbLibraryFilePathName.Text.Substring(0, tbLibraryFilePathName.Text.LastIndexOf('\\'));
+			if (dialog.ShowDialog() == true)
+				tbLibraryFilePathName.Text = dialog.FileName;
+		}
+
+		private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+		}
+	}
+}
